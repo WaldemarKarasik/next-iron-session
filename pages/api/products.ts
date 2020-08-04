b7643436951dcbd0814f24fd080099ff44026761
WaldemarKarasik <waldemarkarasik@gmail.com>
2020-08-05 07:53:21 +0800

    fixed the 500 error

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,7 +1,17 @@
 import withSession from "../../lib/session";
 
-const mongoose = require("mongoose");
-const ProductSchema = require("../../db/schemas/ProductSchema");
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  category: { type: mongoose.SchemaTypes.ObjectId, ref: "Category" },
+  brand: String,
+  image: String,
+  price: Number,
+  rating: Number,
+  numReviews: Number,
+  countInStock: Number,
+});
 
 mongoose.connect(
   "mongodb+srv://komsomolradio:irkytsk87@cluster0.melez.gcp.mongodb.net/vercel-amazemo?retryWrites=true&w=majority",
@@ -17,9 +27,9 @@ let Product;
 try {
   // Trying to get the existing model to avoid OverwriteModelError
 
-  Product = mongoose.model("Product", ProductSchema);
-} catch {
   Product = mongoose.model("Product");
+} catch {
+  Product = mongoose.model("Product", ProductSchema);
 }
 
 export default withSession(async (req, res) => {
